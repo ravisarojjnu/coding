d290de2863265552fd4a220463a60e3b8d2f85df
Ravi Saroj <ravisarojjnu@gmail.com>
2023-10-10 18:48:57 +0530

    segment tree

---
+++ segment-tree/sum_tree.py
@@ -0,0 +1,99 @@
+# Segment Tree Node
+class SegmentTreeNode:
+    def __init__(self, left_index, right_index):
+        self.val = None
+        self.left_index = left_index
+        self.right_index = right_index
+        self.left_child = None
+        self.right_child = None
+
+# Segment Tree Defination
+
+
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.tree = self._build(arr, 0, self.n-1)
+
+    def _build(self, arr, left_index, right_index):
+        if left_index > right_index:
+            return
+
+        node = SegmentTreeNode(left_index, right_index)
+        if left_index == right_index:
+            node.val = arr[left_index]
+        else:
+            mid = (left_index+right_index)//2
+            node.left_child = self._build(arr, left_index, mid)
+            node.right_child = self._build(arr, mid+1, right_index)
+            node.val = node.left_child.val+node.right_child.val
+        return node
+
+    def query(self, l, r):
+        return self._query(l, r, self.tree)
+
+    def _query(self, l, r, node):
+        if node == None:
+            return 0
+        if node.left_index >= l and node.right_index <= r:
+            return node.val
+        lval = self._query(l, r, node.left_child)
+        rval = self._query(l, r, node.right_child)
+        return lval+rval
+
+    def inorder_traversal(self):
+        if self.tree == None:
+            return
+        self._inorder_traversal(self.tree)
+
+    def _inorder_traversal(self, node):
+        if node == None:
+            return
+
+        self._inorder_traversal(node.left_child)
+        print(node.val, " ", end="")
+        self._inorder_traversal(node.right_child)
+
+    def level_order_traversal(self):
+        queue = []
+        queue.append(self.tree)
+        while len(queue) != 0:
+            node = queue.pop(0)
+            print(node.val, " ", end="")
+            if node.left_child:
+                queue.append(node.left_child)
+            if node.right_child:
+                queue.append(node.right_child)
+
+
+if __name__ == "__main__":
+    # Create an array 'arr' with values [1, 2, 3, 4, 5, 6]
+    arr = [1, 2, 3, 4, 5, 6]
+
+    # Create a Segment Tree 's_tree' using the 'arr' as input
+    s_tree = SegmentTree(arr)
+    print("Sum Segment Tree created with input array:", arr)
+
+    # Perform an inorder traversal of the Segment Tree
+    s_tree.inorder_traversal()
+    print("Inorder Traversal of the Segment Tree:")
+
+    # Perform a level order traversal of the Segment Tree
+    print("Level Order Traversal of the Segment Tree:")
+    s_tree.level_order_traversal()
+    print()
+
+    # Print the value stored at the root of the Segment Tree
+    print("Value at the root of the Segment Tree:", s_tree.tree.val)
+
+    # Query the Segment Tree to find the sum of elements in the range [0, 3]
+    val = s_tree.query(0, 3)
+    print("Query [0, 3] Result:", val)
+
+    # Query the Segment Tree to find the sum of elements in the range [2, 5]
+    val = s_tree.query(2, 5)
+    print("Query [2, 5] Result:", val)
+
+    # Query the Segment Tree to find the sum of elements in the range [5, 6]
+    val = s_tree.query(5, 6)
+    print("Query [5, 6] Result:", val)
